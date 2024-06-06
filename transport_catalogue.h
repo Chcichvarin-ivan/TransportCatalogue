5304fc53791b39c9357abf75c784d1c748a0e8b7
Ivan Chichvarin <ichichvarin@humanplus.ru>
2024-06-06 23:11:00 +0300

    code review 06_06

--- transport_catalogue.h
+++ transport_catalogue.h
@@ -2,7 +2,7 @@
  * @Author: Ivan Chichvarin ichichvarin@humanplus.ru
  * @Date: 2024-05-26 00:22:46
  * @LastEditors: Ivan Chichvarin ichichvarin@humanplus.ru
- * @LastEditTime: 2024-06-06 00:04:03
+ * @LastEditTime: 2024-06-06 23:10:06
  * @FilePath: /TransportCatalogue/transport_catalogue.h
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -32,7 +32,7 @@ public:
     static constexpr std::string_view stop_cmd = {"Stop"};
     static constexpr std::string_view bus_cmd = {"Bus"};
 
-    void AddStop(const Stop&& in_stop);
+    void AddStop(const Stop&& in_stop);//не хватит потому что в cpp файле используется семнатика перемещения
     void AddBus(const Bus&& in_bus);
 
     Stop* FindStop(std::string_view in_stop_name);//нужна для заполнения вектора автобусов
@@ -41,7 +41,9 @@ public:
 
     //обработка запросов к базе данных вроде бы не входила в интерфейс класса справочника
     //по крайней мере так я понял из вебинара и задания
-    std::unordered_set<const Stop*> GetUniqueStops(const Bus* bus)const;
+    // по условию задачи печать была вынесена в отельный модуль, я не понимаю зачем мне тащить форматирование внутрь класса
+    // разделить реализации это осознанный выбор
+    std::unordered_set<const Stop*> GetUniqueStops(const Bus* bus)const;// эта функция сделана по итогу анализа вебинара как задел для вычисения расстояния не по дуге сферы, я могу ее удалить но не понимаю зачем.
     std::unordered_set<const Bus*> GetUniqueBuses(const Stop* stop)const; 
     size_t GetUniqueStopsCount(const Bus* bus)const;
     double ComputeRouteLength(const Bus* bus)const;
@@ -51,5 +53,6 @@ private:
     std::deque<Bus> buses_;
     std::unordered_map<std::string_view, Bus*> name_to_bus_;
     // не очень понимаю что мне даст еще один неупорядоченный словарь, да мы делали так в задаче про автобусные расписания, но там структура была частью выполенния запроса, здесь же мы получаем дублирование памяти
+    // эти данные и так хранятся в структуре остановки (строчка 22) и указатель на остановку я получаю из словаря name_to_stop, в чем прирост эфективности?
 };
 }//namespace transport_catalogue 
